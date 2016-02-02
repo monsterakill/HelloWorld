264109a8b1b0a78443ae13a2132d5221747905d7
Slava <monsterakill@mail.ru>
2016-02-02 17:00:18 +0200

    HotFix

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,6 @@ import javax.swing.SwingUtilities;
 public class Main{
 
     public static void main(String[] args) throws IOException {
-        System.out.print("ello orld");
+        System.out.print("Hello World");
     }
 }
